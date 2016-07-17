6d5d33c5068c0cf40f6c3360898e55e497bffa87
hashans <hashans@yahoo.com>
2016-07-17 21:24:00 +0530

    Initial submission

---
+++ server.js
@@ -0,0 +1,35 @@
+/**
+ * Created by hashan on 7/17/16.
+ */
+var mosca = require('mosca');
+
+var ascoltatore = {
+    //using ascoltatore
+    type: 'mongo',
+    url: 'mongodb://localhost:27017/mqtt',
+    pubsubCollection: 'ascoltatori',
+    mongo: {}
+};
+
+var settings = {
+    port: 6001,
+    backend: ascoltatore
+};
+
+var server = new mosca.Server(settings);
+
+server.on('clientConnected', function(client) {
+    console.log('client connected', client.id);
+});
+
+// fired when a message is received
+server.on('published', function(packet, client) {
+    console.log('Published', packet.payload);
+});
+
+server.on('ready', setup);
+
+// fired when the mqtt server is ready
+function setup() {
+    console.log('Mosca server is up and running');
+}
